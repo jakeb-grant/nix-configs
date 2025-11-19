3293fc616146ffd5d904be23d9c4ccb5b779e3aa
jgrant <86214494+jakeb-grant@users.noreply.github.com>
2025-11-19 16:33:35 -0700

    Update git.nix
    Simplifying the configuration.

--- modules/home/programs/git.nix
+++ modules/home/programs/git.nix
@@ -4,34 +4,8 @@
   programs.git = {
     enable = true;
 
-    extraConfig = {
+    settings = {
       user.name = "Your Name";  # Change this
       user.email = "your.email@example.com";  # Change this
-
-      init.defaultBranch = "main";
-      pull.rebase = true;
-      core.editor = "vim";
-
-      # Aliases
-      alias = {
-        st = "status";
-        co = "checkout";
-        br = "branch";
-        ci = "commit";
-        unstage = "reset HEAD --";
-        last = "log -1 HEAD";
-        lg = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
-      };
     };
-
-    # Delta for better diffs
-    delta = {
-      enable = true;
-      options = {
-        navigate = true;
-        line-numbers = true;
-        side-by-side = false;
-      };
-    };
-  };
 }
